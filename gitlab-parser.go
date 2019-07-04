3c094ff8d8ebe8a8695a2dbd44958e3a66315b1d
Stefano Pirrello <spirrello@liaison.com>
2019-07-03 22:29:12 -0400

    parsing json file with struct

--- gitlab-parser.go
+++ gitlab-parser.go
@@ -3,49 +3,58 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"reflect"
+	"time"
 )
 
+//Request struct for parsing requests from api_json.log
+type Request struct {
+	Time     time.Time `json:"time"`
+	Severity string    `json:"severity"`
+	Duration float64   `json:"duration"`
+	Db       float64   `json:"db"`
+	View     float64   `json:"view"`
+	Status   int       `json:"status"`
+	Method   string    `json:"method"`
+	Path     string    `json:"path"`
+	Params   struct {
+		Filepath string `json:"filepath"`
+	} `json:"params"`
+	Host string `json:"host"`
+	IP   string `json:"ip"`
+	Ua   string `json:"ua"`
+}
+
 //getJSONFileData extracts the JSON formated data from the file
-func getJSONFileData(apiJSONFileName string) []map[string]interface{} {
+func getJSONFileData(apiJSONFileName string) { //[]Request {
 
 	//creat slice for collecting JSON objects
-	var jsonFileSlice []map[string]interface{}
+	//var jsonFileSlice []map[string]interface{}
+	//var jsonFileSlice []Request
 
 	// Open our jsonFile
-	apiJSONFile, err := os.Open(apiJSONFileName)
-	// if we os.Open returns an error then handle it
+	//apiJSONFile, err := os.Open(apiJSONFileName)
+	apiJSONFile, err := ioutil.ReadFile(apiJSONFileName)
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	// defer the closing of our jsonFile so that we can parse it later on
-	defer apiJSONFile.Close()
 
-	scanVar := bufio.NewScanner(apiJSONFile)
+	for _, line := range bytes.Split(apiJSONFile, []byte{'\n'}) {
+		var request Request
 
-	//Loop and append each line to a slice
-	for scanVar.Scan() {
-		var result map[string]interface{}
-		if err := json.Unmarshal(scanVar.Bytes(), &result); err != nil {
-			fmt.Println("JSON PARSING FAILED")
-		}
-		//fmt.Println(reflect.TypeOf(result))
-		//fmt.Println("Appending:", result["path"])
+		json.Unmarshal([]byte(line), &request)
+		//fmt.Println(err)
 
-		jsonFileSlice = append(jsonFileSlice, result)
-
+		fmt.Println(request)
 	}
 
-	if scanVar.Err() != nil {
-		fmt.Println("scanVar.Scan() failed in getJSONFileData")
-	}
-
-	return jsonFileSlice
+	//return jsonFileSlice
 
 }
 
@@ -59,13 +68,15 @@ func main() {
 	apiJSONFileName := flag.String("apijsonlog", "api_json.log", "Secret file in json format, 'user', 'secret'")
 
 	//Extract json data from the file
-	apiFileData := getJSONFileData(*apiJSONFileName)
+	//apiFileData := getJSONFileData(*apiJSONFileName)
+	getJSONFileData(*apiJSONFileName)
 
+	//COMMENTING OUT FOR NOW
 	//Loop through the returned file data
-	for _, value := range apiFileData {
-		println(value["path"])
-		//Print
-		fmt.Println(reflect.TypeOf(value))
-	}
+	// for value := range apiFileData {
+	// 	println(value.path)
+	// 	//Print
+	// 	//fmt.Println(reflect.TypeOf(value))
+	// }
 
 }
